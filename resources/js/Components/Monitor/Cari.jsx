1f18d7daa9dd939cc4f84ac13e570e4b854be920
xsaatu <x.one.gmg1@gmail.com>
2023-09-07 21:20:54 +0700

    visual search function

--- resources/js/Components/Monitor/Cari.jsx
+++ resources/js/Components/Monitor/Cari.jsx
@@ -1,10 +1,37 @@
 const isSearch = (produk) => {
     return produk.map((data, i) => {
-        return
+        return <div key={i} className="stats shadow">
+        <div className="stat place-items-center">
+          <div className="stat-title">Nomor Produk</div>
+          <div className="stat-value">{data.so}</div>
+        </div>
+        
+        <div className="stat place-items-center">
+          <div className="stat-title">Nama</div>
+          <div className="stat-value text-secondary">{data.name}</div>
+        </div>
+        
+        <div className="stat place-items-center">
+          <div className="stat-title">Estimasi</div>
+          <div className="stat-value">{data.est}</div>
+        </div>
+        
+      </div>
     })
 }
 
+const noProduct = () => {
+    return (
+      <div></div>
+    )
+}
+  
+const ProductSearch = ({ produk }) => {
+    return !produk ? noProduct() : isSearch(produk)
+}
 
+
+export default ProductSearch;
 
 // export default function Cari(props) {
 //     <table border="1" cellpadding="10" cellspacing="0">
--- resources/js/Pages/Monitor.jsx
+++ resources/js/Pages/Monitor.jsx
@@ -1,5 +1,5 @@
 import ProductLists from "@/Components/Monitor/ProductList";
-// import Cari from "@/Components/Monitor/cari";
+import  ProductSearch from "@/Components/Monitor/cari";
 import Navbar from "@/Components/Navbar";
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from "@inertiajs/react";
@@ -18,22 +18,45 @@ export default function Monitor(props) {
         <Head title="Monitor"/>
 
         {/* Search bar */}
-        <div className="mt-5 max-w-2xl mx-auto sm:px-6 lg:px-4">
-            <form action="/product/search" method="GET" className="form-inline">
-                <div className="flex flex-wrap justify-content-start items-start">
-                <input type="search" placeholder="Search" name="search" className="input input-bordered w-auto" />
-                <button className="btn btn-ghost btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                </button>
-                </div>
-            </form>
+        <div className="container flex-none">
+            <div className="mt-5 mx-auto sm:px-6 lg:px-4">
+                <form action="/product/search" method="GET" className="form-inline">
+                    <div className="flex flex-wrap ">
+                    <div className="input-group justify-end">
+                        <input type="search" placeholder="Search SO" name="search" className="input input-bordered" />
+                        <button className="btn btn-square">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        </button>
+                    </div>
+                    </div>
+                </form>
+            </div>
         </div>
 
         <div className='flex justify-center flex-col lg:flex-row lg:flex-wrap lg:items-stretch items-center gap-4 p-4'>
             {/* <ProductLists product={props.product.data} /> */}
-            {/* <Cari /> */}
+                <ProductSearch produk={props.produk}/>
+
         </div>
             </AuthenticatedLayout>
         </>
     )
 }
+
+
+
+
+{/* Search */}
+            {/* <table border="1" cellpadding="10" cellspacing="0">
+                <tr>
+                    <th>NP</th>
+                    <th>Nama</th>
+                    <th>Proses</th>
+                    <th>Estimasi</th>
+                </tr>
+            </table> */}
+// Button
+        {/* <input type="search" placeholder="Search" name="search" className="input input-bordered w-auto" />
+            <button className="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+        </button> */}
